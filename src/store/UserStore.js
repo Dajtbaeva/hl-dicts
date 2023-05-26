3a1052756e64a8b8257928d3250c687e650f6cd8
Darina <dajtbaeva@gmail.com>
2023-05-26 23:39:31 +0600

    upd

---
+++ src/store/UserStore.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { ref } from "vue";
+
+export const useUserStore = defineStore("userStore", () => {
+  const dicts = ref([]);
+  const loading = ref(false);
+  const URL =
+    "https://products.halyklife.kz/api/v1/test/insis/Arm/api/Dictionary/GetDictionaryList";
+
+  const getDicts = async () => {
+    try {
+      loading.value = true;
+      const response = await axios.get(`${URL}`);
+      dicts.value = response.data.results;
+    } catch (err) {
+      console.log("Error from SearchStore: " + err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    getDicts,
+    dicts,
+  };
+});
